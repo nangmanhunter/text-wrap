8b6dd706a3dbdc85118dac662c9e3efb7ce99452
djwlfdls1 <djwlfdls1@gmail.com>
2026-06-21 03:04:14 +0900

    add: text-wrap-...

--- src/extension.ts
+++ src/extension.ts
@@ -3,9 +3,21 @@ import { textWrapHandler} from './functions/textWrapHandler';
 
 export function activate(context: vscode.ExtensionContext) {
 
-    let disposable = vscode.commands.registerCommand('text-wrap-quotes-double', textWrapHandler('"','"'));
+	const disposables: vscode.Disposable[] = [
+        
+        vscode.commands.registerCommand('text-wrap-quotes-double', textWrapHandler('"', '"')),
+        vscode.commands.registerCommand('text-wrap-quotes-double-comma', textWrapHandler('"', '",')),
+        vscode.commands.registerCommand('text-wrap-quotes-single', textWrapHandler("'", "'")),
+        vscode.commands.registerCommand('text-wrap-backtick', textWrapHandler('`', '`')),
+        vscode.commands.registerCommand('text-wrap-brackets-round', textWrapHandler('(', ')')),
+        vscode.commands.registerCommand('text-wrap-brackets-square', textWrapHandler('[', ']')),
+        vscode.commands.registerCommand('text-wrap-brackets-curly', textWrapHandler('{', '}')),
+        vscode.commands.registerCommand('text-wrap-brackets-angle', textWrapHandler('<', '>')),
+        
+    ];
 
-    context.subscriptions.push(disposable);
+
+	context.subscriptions.push(...disposables);
 }
 
 export function deactivate() {}
